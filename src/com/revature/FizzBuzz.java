61e0f483edbe0350189afb78285e3b4e11cf9eec
Developer <josephdg3@gmail.com>
2018-09-25 10:13:36 -0500

    This is the initial commit to my project.

---
+++ src/com/revature/FizzBuzz.java
@@ -0,0 +1,15 @@
+package com.revature;
+
+/**
+*	This is a basic FizzBuzz attempt. 
+*
+*	This program will run FizzBuzz. You can run this following the
+*	directions in the README.
+*
+*	@author Joseph Gonzalez
+*/
+class FizzBuzz {
+	public static void main (String[] args) {
+		//TODO: FizzBuzz
+	}
+}
